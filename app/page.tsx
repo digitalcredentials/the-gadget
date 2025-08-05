1cb3a0851b437c9b44c605a5b2a54ff9fdf72629
James Chartrand <jc.chartrand@gmail.com>
2025-08-05 07:16:00 -0400

    add logout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { PowerIcon } from '@heroicons/react/24/outline';
+import { signOut } from '@/auth';
 
 export default function Home() {
   return (
@@ -20,7 +22,17 @@ export default function Home() {
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
        
-      
+              <form
+          action={async () => {
+            'use server';
+            await signOut({ redirectTo: '/' });
+          }}
+        >
+          <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <PowerIcon className="w-6" />
+            <div className="hidden md:block">Sign Out</div>
+          </button>
+        </form>
       </footer>
     </div>
   );
