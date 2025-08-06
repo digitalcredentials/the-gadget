5d065cbdda3ff6ff6295c5f6e7024ebf632fa6a1
James Chartrand <jc.chartrand@gmail.com>
2025-08-06 11:44:10 -0400

    pass tenant and token to deeplink creation

--- app/lib/deepLink.ts
+++ app/lib/deepLink.ts
@@ -1,10 +1,8 @@
 'use server';
 
 const exchangeHost = process.env.EXCHANGE_HOST
-const tenantAuthToken = process.env.LCWEXP_TOKEN
-
 const timeToLive = 300000  // 5 minutes
-const tenantName = 'test'
+
 
 export async function confirmDeepLinkStillValid(deepLink:string):Promise<boolean> {
   const parsedDeepLink = new URL(deepLink)
@@ -14,7 +12,10 @@ export async function confirmDeepLinkStillValid(deepLink:string):Promise<boolean
   return result.verifiablePresentationRequest
 }
 
-export async function getDeepLink(vc:any):Promise<any> {
+export async function getDeepLink(vc:any, tenantName:string, tenantAuthToken:string):Promise<any> {
+
+  //const tenantName = 'test'
+//const tenantAuthToken = process.env.LCWEXP_TOKEN
 
   const dataToPost = {
     tenantName,
@@ -29,12 +30,12 @@ export async function getDeepLink(vc:any):Promise<any> {
   }
   // deeplinks look like this:
   //https://lcw.app/request.html?issuer=issuer.example.com&auth_type=bearer&challenge=50991c0d-e033-49c4-86aa-7f3620cf6937&vc_request_url=https://issuer.dcconsortium.org/exchange/e63007bc-6065-417c-8ae8-6b8fbc6a79df/50991c0d-e033-49c4-86aa-7f3620cf6937
-    const result = await postData(`${exchangeHost}/exchange/setup`, dataToPost)
+    const result = await postData(`${exchangeHost}/exchange/setup`, dataToPost, tenantAuthToken)
     const deepLink = result[0].directDeepLink;
     return deepLink     
 }
 
-async function postData(url = "", data = {}) {
+async function postData(url = "", data = {}, tenantAuthToken:string) {
   const response = await fetch(url, {
     method: "POST",
     mode: "cors",
