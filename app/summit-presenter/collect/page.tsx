6c46cd0ba09281182794332e7c903115fbd90d2b
James Chartrand <jc.chartrand@gmail.com>
2025-08-14 12:32:20 -0400

    update collect page content

--- app/summit-presenter/collect/page.tsx
+++ app/summit-presenter/collect/page.tsx
@@ -12,9 +12,11 @@ async function DeepLinks({ deepLink, recipientName }: { recipientName: string, d
 
   return (
     <div className="flex flex-col gap-3 m-10">
-
+      <div className="max-w-[900px] text-left text-l md:text-3xl font-medium mb-5">{`Hello ${recipientName}!`}</div>
       <div className="max-w-[900px] text-left text-sm md:text-lg font-medium">
-        {`${recipientName}, this is where you can claim your DCC Summit Presenter Badge. Here’s how it works:`}
+        This is where you can claim your DCC Summit Presenter Badge.</div>
+        <div className="max-w-[900px] text-left text-sm md:text-lg font-medium">
+          Here’s how it works:
       </div>
       <div className="max-w-[900px] text-left text-sm md:text-base font-medium">
         1. If you haven't installed the Learner Credential Wallet yet, <Link className="font-extrabold text-gray-800 dark:text-white transition-colors hover:bg-gray-200" href="https://lcw.app/" target="_blank">install on your mobile device</Link>, follow set-up instructions in the app and return here for next steps.
@@ -50,6 +52,62 @@ async function DeepLinks({ deepLink, recipientName }: { recipientName: string, d
       <div className="max-w-[900px] text-left text-sm md:text-base font-medium">
       5. If you would like to share your badge on the web, click on your badge and look for three vertical dots in the top right corner. When you click on the dots, you will see the option to share. This will take you to a screen where you can choose to create a public url to share your badge with anyone and an option to post the badge on your LinkedIn profile. 
       </div>
+
+
+
+            <div className="max-w-[800px] text-base md:text-base font-bold mt-8 md:mt-10 mb-1 md:mb-1" id="explainer">
+        How does this work?
+      </div>
+
+      <div className="max-w-[800px] text-sm md:text-base font:medium flex flex-col gap-3 space-y-2.5">
+        <p>
+        The Learner Credential Wallet will create a default profile with a digital identity called a Decentralized Identifier (DID). When you claim your badge – by clicking on a link or scanning the QR code with your device’s camera or the from the LCW’s “Add Credential” screen (“Scan QR code” button) – the link and QR code will open the LCW. Almost instantaneously you will be asked if you want to accept the LCW badge.
+        </p>
+
+        <p>
+       When you click on the link to accept, the DCC issuing system sends a request to the wallet that tells it that it has a credential to send. The wallet sends the DID for your profile back to the issuing system. Then the issuing system compiles the awarded badge with your DID, digitally signs the badge, and sends it to the wallet for you to accept.    </p>
+
+        <p>
+          When you accept it, you will see a green check mark indicating that it has been verified. This means that:
+        </p>
+
+        <ul className="list-disc ml-5 p-1">
+        <li>the badge was signed properly</li>
+        <li>the badge data hasn’t been changed since it was signed.</li>
+</ul>
+
+        <p>
+          The LCW also checks that
+        </p>
+
+        <ul className="list-disc ml-5 p-1">
+          <li>the identity issuer of the badge has been substantiated (that is a DID too!)</li>
+          <li>the badge hasn’t been revoked</li>
+          <li>it hasn’t expired</li>
+        </ul>
+
+        <p>
+        Your badge doesn’t exist until you request it. Once it is in your wallet, it is yours to share with others however you choose.
+        </p>
+
+        <p>
+         To share your badge, click “Done” and go to the home screen. Click on your badge and look for three vertical dots in the top right corner. When you click on the dots, you will see the option to share. This will take you to a screen where you can choose to create a public url to share your badge with anyone and an option to post the badge on your LinkedIn profile.
+        </p>
+
+        <p className='font-extrabold'>
+          Note about Privacy:
+        </p>
+
+        <p>
+         The DCC does not save any of your information when creating and issuing this badge. The LCW does not track any use of the app or where you share your public link. 
+         </p><p>
+The link is hosted at our web verifier app called VerifierPlus where your badge can be verified by anyone in real-time. This website will hold the badge data for as long as the link exists. However, you can destroy the link at any time – by locating it on the share screen for your badge – and this will delete it from VerifierPlus too. 
+ </p><p>
+As with the LCW, VerifierPlus does not track where you share this link. The DCC believes that we should all have unfettered access to our data and be able to share it privately. So, we try our best to make that possible with our software.
+     </p>
+
+      </div>
+
     </div>)
 }
 
