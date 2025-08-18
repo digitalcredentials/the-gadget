08a27181aae5b76f24d4b99037ba033458cf8e30
James Chartrand <jc.chartrand@gmail.com>
2025-08-18 15:01:11 -0400

    fix existing tests

--- tests/gadget.spec.ts
+++ tests/gadget.spec.ts
@@ -1,40 +1,39 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-//  await page.goto('https://playwright.dev/');
-//await page.goto('http://localhost:3000')
 
-  // Expect a title "to contain" a substring.
- // await expect(page).toHaveTitle('Choose a Credential | The Gadget');
+await page.goto('http://localhost:3000')
+await expect(page).toHaveTitle('Choose a Credential | The Gadget');
 });
 
 test('opens login page when not logged in', async ({ page }) => {
-  //await page.goto('http://localhost:3000');
+  await page.goto('http://localhost:3000');
 
   // Click the Summit Presenter link
-  //await page.getByRole('link', { name: '' }).click();
+  await page.getByRole('link', { name: '' }).click();
 
-   //await expect(page.getByText('Please log in to issue some credentials.')).toBeVisible();
+  await expect(page.getByText('Please log in to issue some credentials.')).toBeVisible();
 
-  // Expects page to have a heading with the name of Installation.
-  //await expect(page.getByRole('heading', { name: 'DCC Summit Presenter Badge' })).toBeVisible();
+
 });
 
 test('logs in', async ({ page }) => {
-  //await page.goto('http://localhost:3000/login');
+  await page.goto('http://localhost:3000/login');
 
   //const username = page.getByTestId
 
-   //await page.getByLabel('Email').fill('chartraj@mit.edu');
+  await page.getByLabel('Email').fill('chartraj@mit.edu');
+  await page.getByLabel('Password').fill('thelake');
 
-//await page.getByLabel('Password').fill('thelake');
+  await page.getByRole('button', { name: 'Log in' }).click();
 
-//await page.getByRole('button', { name: 'Log in' }).click();
-
- //await expect(page).toHaveTitle('Choose a Credential | The Gadget');
-//await expect(page.getByText('Choose a credential to issue')).toBeVisible();
-//await expect(page.getByRole('link', { name: 'DCC Summit Presenter' })).toBeVisible();
+ await expect(page).toHaveTitle('Choose a Credential | The Gadget');
+ await expect(page.getByText('Choose a credential to issue')).toBeVisible();
+ await expect(page.getByRole('link', { name: 'DCC Summit Presenter' })).toBeVisible();
 });
+
+//  await expect(page.getByRole('heading', { name: 'DCC Summit Presenter Badge' })).toBeVisible();
+
 
 /* make a test that creates a cred
   - add email and person name, and click Button.
